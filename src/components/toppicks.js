f61d162eac83babbe79236d76f655cfb782c87a8
Jessica Long <35347408+Jessicalong627@users.noreply.github.com>
2018-11-27 12:28:41 -0500

    Update toppicks.js

--- src/components/toppicks.js
+++ src/components/toppicks.js
@@ -9,13 +9,13 @@ function TopPicks(props) {
      <React.Fragment>
         <h1 className="top-picks">Top Picks</h1>
         <main className="home-top-picks">
-          <section className="home-top-pick"><img src={cherry} className="App-logo" alt="logo" /><p className="home-name">Strawberry & Peach</p><p className="home-kind">Smoothie</p><NavLink
+          <section className="home-top-pick"><img src={cherry} className="App-logo" alt="strawberry smoothie" /><p className="home-name">Strawberry & Peach</p><p className="home-kind">Smoothie</p><NavLink
           className="home-products"
           to="/products"> See all products </NavLink></section>
-        <section className="home-top-pick"><img src={chocolate} className="App-logo" alt="logo" /><p className="home-name">Chocolate</p><p className="home-kind">Harvest Bowl</p><NavLink
+        <section className="home-top-pick"><img src={chocolate} className="App-logo" alt="chocolate smoothie" /><p className="home-name">Chocolate</p><p className="home-kind">Harvest Bowl</p><NavLink
           className="home-products"
           to="/products"> See all products </NavLink></section>
-        <section className="home-top-pick"><img src={acai} className="App-logo" alt="logo" /><p className="home-name">Acai & Raspberry</p><p className="home-kind">Smoothie</p><NavLink
+        <section className="home-top-pick"><img src={acai} className="App-logo" alt="acai and raspberry smoothie" /><p className="home-name">Acai & Raspberry</p><p className="home-kind">Smoothie</p><NavLink
           className="home-products"
           to="/products"> See all products </NavLink></section>
       </main>
