20df32251a66cadc46dca2c5fbd3dbd06bcf8863
Jessica Long <35347408+Jessicalong627@users.noreply.github.com>
2018-11-27 12:32:56 -0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,8 +4,8 @@ import logo from '../css/assets/whitelogo.png';
 function Footer(props) {
   return (
       <footer className="footer">
-        <main id="footer-image" className="footer-image">
-            <img src={logo} className="footer-logo" alt="logo" />
+        <main id="footer-image" className="raspberry-photo">
+            <img src={logo} className="footer-logo" alt="smoothie-logo" />
         </main>
       </footer>
   );
